466f9203ea8ef816cb7c584740d27d4717f60560
GilbertII <gilbert.abuan.ii@gmail.com>
2021-07-23 23:16:09 +0800

    Project Object Validation part1

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -1,8 +1,11 @@
 package io.agileintelligence.ppmtool.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +22,12 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<? extends Object> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()){
+            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
+
         projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
