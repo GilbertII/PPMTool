f33eee9f68c947a460ab7e40085d25800ab7efd0
GilbertII <gilbert.abuan.ii@gmail.com>
2022-05-03 01:33:51 +0800

    Bringing Bootstrap from 4 to 5

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/layout/Header";
 import Dashboard from "./components/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
